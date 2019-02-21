0af729f394c1fdb5366afe7434f676df16b7231b
wuling31715 <wuling31715@gmail.com>
2019-02-21 14:25:28 +0800

    test

---
+++ ch01/test.c
@@ -0,0 +1,11 @@
+/* prog1_1, �Ĥ@��C�{���X */ 
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
+{
+   printf("Hello C!\n");   	/* �L�XHello C! �r�� */
+   printf("Hello World!\n");   /* �L�XHello World! �r�� */   
+   
+   system("pause");
+   return 0;
+}
