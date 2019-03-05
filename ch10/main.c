d1ef4759cd26af3845329f52d49fab5fec470e2b
蕭擎軒 <wuling31715@xiaoqingxuande-MacBook-Air.local>
2019-03-05 18:52:40 +0800

    ch10

--- ch10/main.c
+++ ch10/main.c
@@ -3,6 +3,9 @@
 
 int main(void)
 {
-    printf("Hello World!");
+    int a = 0;
+    int *ptr = &a;
+    printf("a = %d, address of a = %p\n", a, &a);
+    printf("p = %d, address of a = %p, address of p = %p\n", *ptr, ptr, &ptr);
     return 0;
 }
