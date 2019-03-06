f39f9f624d81b30b633248f2a68206e34ff8b7d4
蕭擎軒 <wuling31715@xiaoqingxuande-MacBook-Air.local>
2019-03-06 18:36:09 +0800

    ch12

--- ch12/main.c
+++ ch12/main.c
@@ -1,8 +1,19 @@
-# include<stdio.h>
-# include<stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
 
 int main(void)
 {
-    printf("Hello World!");
+    FILE *fptr, *copy;
+    fptr = fopen("test.txt", "r");
+    copy = fopen("copy.txt", "a");    
+    char c;
+    int sum;
+    while((c = getc(fptr)) != EOF){    
+        sum ++;
+        printf("%c", c);
+        putc(c, copy);
+    }
+    fclose(fptr);
+    printf("\nthere are %d char in test.txt\n", sum);
     return 0;
 }
