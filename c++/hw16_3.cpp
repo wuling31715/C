1965dbda3ea18236c668454632bf1b8f22abaa65
蕭擎軒 <wuling31715@xiaoqingxuande-MacBook-Air.local>
2019-03-10 22:02:30 +0800

    c++

---
+++ c++/hw16_3.cpp
@@ -0,0 +1,18 @@
+// hw16_3.cpp
+#include <iostream>  // ��iostream�o���ɮקt�A�i��
+#include <cstdlib>   // ��cstdlib�o���ɮקt�A�i��
+using namespace std;
+int main(void)       // �D���main()�q�o��}�l
+{  
+   int num;				// �ŧi����ܼ�num
+   num=2;		 		// ��num���ȳ]��2
+   cout << "I have "<< num << " cats." << endl; // �L�X�r��Pnum�����e 
+   cout << "You have "<< num << " cats, too." << endl; // �L�X�r��Pnum�����e      
+   system("pause");  // �I�sdos�̪�pause���O�A�ΨӼȰ��{��������
+   return 0;
+}
+
+/* output--------------
+I have 2 cats.
+You have 2 cats, too.
+---------------------*/
--- c++/main.c
+++ c++/main.c
@@ -3,6 +3,6 @@
 
 int main(void)
 {
-    printf("Hello World!");
+    printf("Hello World!\n");
     return 0;
 }
