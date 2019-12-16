896a9017704fe0b53c49ef0c2e9a8abf514cbe16
wuling31715 <wuling31715@gmail.com>
2019-12-16 12:46:46 +0800

    ntu cs

--- advanced/ch04_if/assignment/4.7.c
+++ advanced/ch04_if/assignment/4.7.c
@@ -1,32 +1,26 @@
 #include <stdio.h>
 
-int add(end, counter, sum)
-{
-    if (counter != end)
-        {
-            counter ++;
-            sum += counter;
-            end, counter, sum = add(end, counter, sum);
-        }
-    return end, counter, sum;
-}
-
 int main()
 {
     int start, end, sum, counter;
     scanf("%d", &start);
     scanf("%d", &end);
-    if (end > start)
+    counter = sum = start;
+    if (start < end)
     {
-        counter = sum = start;
-        end, counter, sum = add(end, counter, sum);
-        sum += end;
+        while (counter != end)
+        {
+            counter ++;
+            sum += counter;
+        }
     }
     else
     {
-        counter = sum = end;
-        start, counter, sum = add(start, counter, sum);
-        sum += start;
+        while (counter != end)
+        {
+            counter --;
+            sum += counter;
+        }
     }
-    printf("%d%d%d%d\n", start, end, counter, sum); 
+    printf("%d\n", sum);
 }
