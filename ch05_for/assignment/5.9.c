c14e99dce51e26c89d7f66fd3a47061c16ac589b
wuling31715 <wuling31715@gmail.com>
2019-12-16 16:56:53 +0800

    .

--- ch05_for/assignment/5.9.c
+++ ch05_for/assignment/5.9.c
@@ -1,22 +1,39 @@
 #include <stdio.h>
 
-int is_prime()
+int get_factor(k)
 {
-    int i, j, k;
+    int i, j;
     j = 1;
-    scanf("%d", &k);
     for (i = 2; (i * i) <= k; i++)
     {
         if (k % i == 0)
             {
-                j = 0;
-                break;
+                return i; // 因數
+                j = 0; // 和數
+                break; 
             }
     }
-    printf("%d\n", j);
+    return 0; // 質數
 }
 
 int main()
 {
-
+    int n, factor, i;
+    int factor_array[10];
+    i = 0;
+    scanf("%d", &n);
+    factor = get_factor(n);
+    while (factor != 0)
+    {
+        factor = get_factor(n);
+        factor_array[i] = factor;
+        i++;
+        n = n / factor;
+        factor = get_factor(n);
+    }
+    factor_array[i] = n;
+    for (i = 0; i < 10; i++)
+    {
+        printf("%d ", factor_array[i]);
+    }
 }
